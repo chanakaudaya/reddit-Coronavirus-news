d4004c8c629614d0825bec926282085ce07cd56a
Chanaka Fernando <chanaka.leadership@gmail.com>
2020-03-12 14:17:22 +0530

    Updates

---
+++ precache-manifest.70c198e87e7c374a38c73ab8b78bfd55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4326bbd6d0eea3406efcdaff8aa73778",
+    "url": "/reddit-coronavirus-news/index.html"
+  },
+  {
+    "revision": "b8a2b595b320567647bb",
+    "url": "/reddit-coronavirus-news/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fe0a2b702c673154a36",
+    "url": "/reddit-coronavirus-news/static/js/2.37229195.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reddit-coronavirus-news/static/js/2.37229195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a2b595b320567647bb",
+    "url": "/reddit-coronavirus-news/static/js/main.06930104.chunk.js"
+  },
+  {
+    "revision": "64592403e663bf1281bf",
+    "url": "/reddit-coronavirus-news/static/js/runtime-main.d59f3ac3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reddit-coronavirus-news/static/media/logo.5d5d9eef.svg"
+  }
+]);
